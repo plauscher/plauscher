b4c0f9a79e49a1101feb2d7f185cc768794ee0bc
Oliver Oehme <oliver@plauscher.de>
2012-01-15 15:30:59 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Plauscher::Application.config.secret_token = '32a6931be4301c8d4393245393f39906704620a677985fefb8c8c30eb5b52e3810d1128b872e0236e2ee71d022dc482621b0fa9ef9699bb5b9428e1e60d3312f'
